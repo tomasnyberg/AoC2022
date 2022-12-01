a8b657fa2e31f041b0eb7f2778a390716371ea79
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-01 11:07:11 +0100

    solve day1 p1 and p2

--- 1.py
+++ 1.py
@@ -1,3 +1,17 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+result = 0
+currtotal = 0
+elves = []
+for line in lines:
+    if line == "":
+        elves.append(currtotal)
+        currtotal = 0
+    else:
+        currtotal+=int(line)
+elves.append(currtotal)
+elves.sort()
+
+print("Part one:", elves[-1])
+print("Part two:", sum(elves[-3:]))
