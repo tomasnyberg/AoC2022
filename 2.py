f444922c06322026a8b29f3adbbe0ed3861454f1
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-02 06:45:41 +0100

    simplify (sort of brute force) day 2

--- 2.py
+++ 2.py
@@ -1,38 +1,13 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-scores = {'X': 1, 'Y': 2, 'Z': 3}
-def part_one():
-    equals = {'X': 'A', 'Y': 'B', 'Z': 'C'}
-    beats = {'Y': 'A', 'Z': 'B', 'X': 'C'}
-    def get_score(a, b):
-        if equals[b] == a:
-            return 3
-        if beats[b] == a:
-            return 6
-        return 0
-    result = 0
-    for line in lines:
-        a, b = line.split()
-        result += get_score(a, b) + scores[b]
-    return result
+p1 = 0
+p2 = 0
+p1d = {'A X': 4, 'B X': 1, 'C X': 7, 'A Y': 8, 'B Y': 5, 'C Y': 2, 'A Z': 3, 'B Z': 9, 'C Z': 6}
+p2d = {'A X': 3, 'B X': 1, 'C X': 2, 'A Y': 4, 'B Y': 5, 'C Y': 6, 'A Z': 8, 'B Z': 9, 'C Z': 7}
+for line in lines:
+    p1 += p1d[line]
+    p2 += p2d[line]
 
-def part_two():
-    p2scores = {'X': 0, 'Y': 3, 'Z': 6}
-    beatsp2 = {'A':'Y', 'B':'Z', 'C':'X'}
-    loses = {'A': 'Z', 'B': 'X', 'C': 'Y'}
-    equalsp2 = {'A':'X', 'B':'Y', 'C':'Z'}
-    result = 0
-    for line in lines:
-        a, b = line.split()
-        result += p2scores[b]
-        if b == 'X':
-            result += scores[loses[a]]
-        elif b == 'Y':
-            result += scores[equalsp2[a]]
-        else:
-            result += scores[beatsp2[a]]
-    return result
-
-print(part_one())
-print(part_two())
+print("Part one:", p1)
+print("Part two:", p2)
