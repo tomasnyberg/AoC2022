b38f0a75933fbae164466664b42aec4439ba3835
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-03 06:42:52 +0100

    steal function for calculating score of char

--- 3.py
+++ 3.py
@@ -1,20 +1,20 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-scores = {chr(i):i-38 for i in range(65,91)}
-for i in range(97, 123):
-    scores[chr(i)] = i-96 
+def getVal(x):
+    return ord(x) - ord('a') + 1 if x.islower() else ord(x) - ord('A') + 27
+
 def part_one():
     result = 0
     for line in lines:
-        result += scores[(set(line[:len(line)//2]) & set(line[len(line)//2:])).pop()]
+        result += getVal((set(line[:len(line)//2]) & set(line[len(line)//2:])).pop())
     return result
 
 def part_two():
     result = 0
     for i in range(0,len(lines), 3):
         ch = (set(lines[i]) & set(lines[i+1]) & set(lines[i+2])).pop()
-        result += scores[ch]
+        result += getVal(ch)
     return result
 print("Part one:", part_one())
 print("Part two:", part_two())
