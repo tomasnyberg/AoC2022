f98fb1862e012bfa7279756d515e37c2da81eaf9
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-06 06:16:05 +0100

    finish day 6, super slow.....

--- 6.py
+++ 6.py
@@ -1,5 +1,6 @@
 import sys, re
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-for line in lines:
-    print(line)
+l, n = lines[0], len(lines[0])
+print("Part one:", [i for i in range(4, n) if len(set(l[i-4:i])) == 4][0])
+print("Part two:", [i for i in range(14, n) if len(set(l[i-14:i])) == 14][0])
