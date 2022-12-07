71a97fead78d2daf329857c268e49d07dc3e1d16
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-07 07:13:53 +0100

    finish up day 7

--- 7.py
+++ 7.py
@@ -3,37 +3,17 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 dirs = {}
 stack = ["$"]
-for line in lines[2:]:
+for line in list(filter(lambda x: x != '$ ls', lines))[2:]:
     split = line.split(" ")
     if line[0] == '$':
-        if split[1] == 'ls': continue
-        if split[2] == '..':
-            stack.pop()
-        else:
-            stack.append(split[2])
-    # elif line[0] == 'd':
+        stack = stack[:-1] if split[2] == '..' else stack + [split[2]]
     elif line[0].isdigit():
         stackcopy = stack[:]
         while stackcopy:
             d = "/".join(stackcopy)
             dirs[d] = dirs.get(d, 0) + int(line.split(" ")[0])
             stackcopy.pop()
-total = 0
-result = 0
-for d in dirs:
-    total += dirs[d]
-    if dirs[d] <= 100000:
-        result += dirs[d]
-print("Part one:", result)
+            
 required = 30000000 - (70000000 - dirs["$"])
-candidates = []
-for d in dirs:
-    if dirs[d] >= required:
-        candidates.append(dirs[d])
-print("Part two:", min(candidates))
-
-
-
-
-
-
+print("Part one:", sum([dirs[d] for d in dirs if dirs[d] <= 100000]))
+print("Part two:", min([dirs[d] for d in dirs if dirs[d] >= required]))
