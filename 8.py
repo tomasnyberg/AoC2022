b9977b75cbdd5ff05a34b86efb8983e87166df70
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-08 16:57:21 +0100

    reduce size of some variable names to not have egregiously long lines

--- 8.py
+++ 8.py
@@ -1,30 +1,30 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
-
 matrix = [[int(y) for y in l] for l in lines]
+n = len(matrix)
 
 def part_one():
     def check(i1, j1):
         return all(matrix[i][j1] < matrix[i1][j1] for i in range(i1-1, -1, -1)) or \
-               all(matrix[i][j1] < matrix[i1][j1] for i in range(i1+1, len(matrix))) or \
+               all(matrix[i][j1] < matrix[i1][j1] for i in range(i1+1, n)) or \
                all(matrix[i1][j] < matrix[i1][j1] for j in range(j1-1, -1, -1)) or \
-               all(matrix[i1][j] < matrix[i1][j1] for j in range(j1+1, len(matrix)))
-    return sum(check(i, j) for i in range(len(matrix)) for j in range(len(matrix)))
+               all(matrix[i1][j] < matrix[i1][j1] for j in range(j1+1, n))
+    return sum(check(i, j) for i in range(n) for j in range(n))
 
 def part_two():
-    def dfs(istart, jstart):
+    def dfs(i1, j1):
         result = 1
-        for updown in [True, False]:
-            for start, end, step in [((istart if updown else jstart)-1, -1, -1), ((istart if updown else jstart)+1, len(matrix), 1)]:
+        for ud in [True, False]:
+            dirsteps = [((i1 if ud else j1)-1, -1, -1), ((i1 if ud else j1)+1, n, 1)]
+            for start, end, step in dirsteps:
                 curr = 0
                 for i in range(start, end, step):
                     curr += 1
-                    if matrix[i if updown else istart][i if not updown else jstart] >= matrix[istart][jstart]:
+                    if matrix[i if ud else i1][i if not ud else j1] >= matrix[i1][j1]:
                         break
                 result *= curr
         return result
-    return max(dfs(i, j) for i in range(len(matrix)) for j in range(len(matrix)))
-
+    return max(dfs(i, j) for i in range(n) for j in range(n))
 
 print("Part one:", part_one())
 print("Part two:", part_two())
