a1f2b275833cfc487d29641a6f46865075ea61ef
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-08 16:16:04 +0100

    solve day 8 p1 in a different way, old way was messy

--- 8.py
+++ 8.py
@@ -1,41 +1,39 @@
 import sys
+from sortedcontainers import SortedList
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+def rotateMatrix(matrix):
+    n = len(matrix)
+    for x in range(0, int(n / 2)):
+        for y in range(x, n-x-1):
+            temp = matrix[x][y]
+            matrix[x][y] = matrix[y][n-1-x]
+            matrix[y][n-1-x] = matrix[n-1-x][n-1-y]
+            matrix[n-1-x][n-1-y] = matrix[n-1-y][x]
+            matrix[n-1-y][x] = temp
+
 matrix = [[int(y) for y in l] for l in lines]
-nums = matrix
-n, m = len(matrix), len(matrix[0])
+scored = [[0 for _ in l] for l in lines]
+for i in range(4):
+    for xs in matrix:
+        print(xs)
+    print()
+    for i in range(len(matrix)):
+        sl = SortedList()
+        for j in range(len(matrix)):
+            if not sl or matrix[i][j] > sl[-1]:
+                scored[i][j] = 1
+            sl.add(matrix[i][j]) 
+    rotateMatrix(matrix)
+    rotateMatrix(scored)
 
-def score(istart, jstart):
-    dirs = [(-1, 0), (1,0), [0, 1], [0, -1]]
-    def dfs(i, j, di, dj):
-        if i < 0 or j < 0 or i == len(matrix) or j == len(matrix[0]) or matrix[i][j] >= matrix[istart][jstart]:
-            return 0
-        return 1 + dfs(i + di, j +dj, di, dj)
-    results = []
-    for di, dj in dirs:
-        results.append(dfs(istart+di, jstart+dj, di, dj))
-    return results
+for xs in scored:
+    print(xs)
 
-def visible(i, j):
-    scores = score(i, j)
-    return scores[0] == i or scores[1] == n - i - 1 or scores[2] == m-j-1 or scores[3] == j
-print(score(0, 0))
-print(visible(0, 0))
+result = 0
+for xs in scored:
+    result += sum(xs)
+print("Part one:", result)
 
-def scenic(i, j):
-    result = 1
-    for x in score(i, j):
-        result *= x
-    return result
 
-p1res = 0
-p2res = 0
-for i in range(len(nums)):
-    for j in range(len(nums[0])):
-        if visible(i, j):
-            print(i, j, "is visible")
-            p1res += 1
-        p2res = max(p2res, scenic(i, j))
-
-print("Part one:", p1res)
-print("Part two:", p2res)
+# print("Part two:", p2res)
