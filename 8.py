664255fca9f06a0cdd7b3b338a9b6dc116fc3cb2
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-08 16:48:04 +0100

    more abstraction

--- 8.py
+++ 8.py
@@ -12,27 +12,6 @@ def rotateMatrix(matrix):
             matrix[y][n-1-x] = matrix[n-1-x][n-1-y]
             matrix[n-1-x][n-1-y] = matrix[n-1-y][x]
             matrix[n-1-y][x] = temp
-            
-def part_two():
-    def dfs(istart, jstart):
-        # up:
-        result = 1
-        for start, end, step in [(istart-1, -1, -1), (istart+1, len(matrix), 1)]:
-            curr = 0
-            for i in range(start, end, step):
-                curr += 1
-                if matrix[i][jstart] >= matrix[istart][jstart]:
-                    break
-            result *= curr
-        for start, end, step in [(jstart-1, -1, -1), (jstart+1, len(matrix[0]), 1)]:
-            curr = 0
-            for j in range(start, end, step):
-                curr += 1
-                if matrix[istart][j] >= matrix[istart][jstart]:
-                    break
-            result *= curr
-        return result
-    return max(dfs(i, j) for i in range(len(matrix)) for j in range(len(matrix)))
 
 def part_one():
     scored = [[0 for _ in l] for l in lines]
@@ -46,6 +25,21 @@ def part_one():
         for m in [matrix, scored]:
             rotateMatrix(m)
     return sum(sum(xs) for xs in scored)
+
+def part_two():
+    def dfs(istart, jstart):
+        result = 1
+        for updown in [True, False]:
+            for start, end, step in [((istart if updown else jstart)-1, -1, -1), ((istart if updown else jstart)+1, len(matrix), 1)]:
+                curr = 0
+                for i in range(start, end, step):
+                    curr += 1
+                    if matrix[i if updown else istart][i if not updown else jstart] >= matrix[istart][jstart]:
+                        break
+                result *= curr
+        return result
+    return max(dfs(i, j) for i in range(len(matrix)) for j in range(len(matrix)))
+
 
 print("Part one:", part_one())
 print("Part two:", part_two())
