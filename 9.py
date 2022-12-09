2d3ac663a46f4533f9170ce4985b88f2ed952a5f
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-09 06:38:49 +0100

    get input in a slightly nicer way

--- 9.py
+++ 9.py
@@ -3,22 +3,15 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 inputs = []
 for line in lines:
-    split = line.split(" ")
-    xs = []
-    xs.append(split[0])
-    xs.append(int(split[1]))
-    inputs.append(xs)
-print(inputs)
-
-
-# diagonally if 3 squares away (like horse in chess)
+    a, b = line.split(" ")
+    inputs.append([a, int(b)])
 
 def movetail(tailpos, headpos):
-    # if the distance between them is 2, move tailpos one step toward headpos
-    # manhattan distance between headpos and tailpos
+    def movetoward(tailpos, headpos, dir):
+        tailpos[dir] += 1 if headpos[dir] > tailpos[dir] else -1
     dist = abs(headpos[0] - tailpos[0]) + abs(headpos[1] - tailpos[1])
     if dist == 2:
-        # If they are on the same row or column, move tailpos one toward headpos
+        dir = 0 if headpos[0] != tailpos[0] else 1
         if headpos[0] == tailpos[0]:
             if headpos[1] > tailpos[1]:
                 tailpos[1] += 1
@@ -30,9 +23,7 @@ def movetail(tailpos, headpos):
             else:
                 tailpos[0] -= 1
     elif dist == 3:
-        # Find out in which direction the headpos is 2 squares from tailpos
         if abs(headpos[0] - tailpos[0]) == 2:
-            # If they are on the same row or column, move tailpos one toward headpos
             if headpos[0] > tailpos[0]:
                 tailpos[0] += 1
             else:
@@ -70,7 +61,6 @@ for i in range(9):
 for d, amount in inputs:
     # print(d, amount)
     for _ in range(amount):
-        print(headpos, tailposes[::-1])
         if d == 'U':
             headpos[0] += 1
         elif d == 'D':
