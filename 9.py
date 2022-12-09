80156e52080d7a6d5304b91fd38d921c1804dc29
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-09 06:28:27 +0100

    do day 9 in a really ugly way

--- 9.py
+++ 9.py
@@ -12,9 +12,7 @@ print(inputs)
 
 
 # diagonally if 3 squares away (like horse in chess)
-visited = set([(0, 0)])
-headpos = [0, 0]
-tailpos = [0, 0]
+
 def movetail(tailpos, headpos):
     # if the distance between them is 2, move tailpos one step toward headpos
     # manhattan distance between headpos and tailpos
@@ -52,11 +50,27 @@ def movetail(tailpos, headpos):
                 tailpos[1] += 1
             else:
                 tailpos[1] -= 1
+    elif dist == 4:
+        # Move toward head in both directions
+        if headpos[0] > tailpos[0]:
+            tailpos[0] += 1
+        else:
+            tailpos[0] -= 1
+        if headpos[1] > tailpos[1]:
+            tailpos[1] += 1
+        else:
+            tailpos[1] -= 1
 
+
+visited = set([(0, 0)])
+headpos = [0, 0]
+tailposes = []
+for i in range(9):
+    tailposes.append([0, 0])
 for d, amount in inputs:
     # print(d, amount)
     for _ in range(amount):
-        # print(headpos, tailpos, visited)
+        print(headpos, tailposes[::-1])
         if d == 'U':
             headpos[0] += 1
         elif d == 'D':
@@ -65,6 +79,8 @@ for d, amount in inputs:
             headpos[1] += 1
         else:
             headpos[1] -= 1
-        movetail(tailpos, headpos)
-        visited.add(tuple(tailpos))
+        movetail(tailposes[-1], headpos)
+        for i in range(len(tailposes) - 1, 0, -1):
+            movetail(tailposes[i-1], tailposes[i])
+        visited.add(tuple(tailposes[0]))
 print(len(visited))
