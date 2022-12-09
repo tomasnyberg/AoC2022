fe5d829aff4f7abb82c3357d3be91f36d23cea07
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-09 07:42:17 +0100

    small changes

--- 9.py
+++ 9.py
@@ -2,20 +2,19 @@ import sys, re
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 inputs = [(a, int(b)) for a, b in [line.split(" ") for line in lines]]
+
 def movetail(tailpos, headpos):
     def movetoward(tailpos, headpos, dir):
         tailpos[dir] += 1 if headpos[dir] > tailpos[dir] else -1
     dist = abs(headpos[0] - tailpos[0]) + abs(headpos[1] - tailpos[1])
     if dist == 2:
         if headpos[0] != tailpos[0] and headpos[1] != tailpos[1]: return 
-        dir = 1 if headpos[0] == tailpos[0] else 0
-        movetoward(tailpos, headpos, dir)
+        movetoward(tailpos, headpos, 1 if headpos[0] == tailpos[0] else 0)
     elif dist == 3 or dist == 4:
         movetoward(tailpos, headpos, 0)
         movetoward(tailpos, headpos, 1)
 
-visitedp1 = set([(0, 0)])
-visitedp2 = set([(0, 0)])
+visitedp1, visitedp2 = [set([(0, 0)]) for _ in range(2)]
 headpos = [0, 0]
 tailposes = [[0, 0] for _ in range(9)]
 mapped = {'U': (0, 1), 'D': (0, -1), 'R': (1, 1), 'L': (1, -1)}
@@ -28,5 +27,6 @@ for d, amount in inputs:
             movetail(tailposes[i-1], tailposes[i])
         visitedp2.add(tuple(tailposes[0]))
         visitedp1.add(tuple(tailposes[-1]))
+
 print("Part one:", len(visitedp1))
 print("Part two:", len(visitedp2))
