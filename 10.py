569ea1e6a92a25147e6bb6a5dbd71d00e5d7dd5e
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-10 07:21:05 +0100

    don't actually need to do the cycle thing

--- 10.py
+++ 10.py
@@ -2,26 +2,27 @@ import sys, re
 lines = list(map(str.strip, sys.stdin.readlines()))
 grid = [["." for _ in range(40)] for _ in range(6)]
 
-def solve():
-    total = [0]
-    iterations = [0]
-    countset = set([20, 60, 100, 140, 180, 220])
-    x = 1
-    for line in lines:
-        def cpu():
-            iterations[0] += 1
-            if iterations[0] in countset:
-                total[0] += x*iterations[0]
-            distance = abs((iterations[0] - 1)%40-x)
-            grid[(iterations[0]-1)//40][(iterations[0]-1)%40] = ["#","."][distance >= 2]
-        if line[0] == 'a':
-            [cpu() for _ in range(2)]
-            x += int(line.split(" ")[1])
-        else:
-            cpu()
-    return total
+total = 0
+iterations = 0
+countset = set([20, 60, 100, 140, 180, 220])
+x = 1
+for line in lines:
+    def between_iterations():
+        distance = abs((iterations - 1)%40-x)
+        grid[(iterations-1)//40][(iterations-1)%40] = ["#","."][distance >= 2]
+        if iterations in countset:
+            return x*iterations
+        return 0
+    if line[0] == 'a':
+        for _ in range(2):
+            iterations += 1
+            total += between_iterations()
+        x += int(line.split(" ")[1])
+    else:
+        iterations +=1
+        total += between_iterations()
 
-print("Part one:", solve()[0])
+print("Part one:", total)
 print("Part two:")
 for xs in grid:
     [print(chr(0x2588) if x == "#" else " ", end="") for x in xs]
