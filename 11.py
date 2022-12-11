c933d27a259669d6a6c14515c57128b7397033d1
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-11 06:20:36 +0100

    do part one

--- 11.py
+++ 11.py
@@ -1,5 +1,54 @@
-import sys
-lines = list(map(str.strip, sys.stdin.readlines()))
+import sys, re
+whole_file = sys.stdin.read()
+monkeys = whole_file.split("\n\n")
 
-for line in lines:
-    print(line)
+monkeylist = []
+for monkey in monkeys:
+    lines = monkey.split("\n")
+    monkeylist.append([])
+    starting = lines[1]
+    # convert this into only numbers Starting items: 76, 66, 74, 87, 70, 56, 51, 66
+    starting = list(map(int,re.findall(r'\d+', starting)))
+    monkeylist[-1].append(starting)
+    op = lines[2].split(": ")[1].split(" = ")[1].split(" ")[1:]
+    monkeylist[-1].append(op)
+    divby = int(re.findall(r'\d+', lines[3])[0])
+    monkeylist[-1].append(divby)
+    truethrow = int(re.findall(r'\d+', lines[4])[0])
+    falsethrow = int(re.findall(r'\d+', lines[5])[0])
+    monkeylist[-1].append(truethrow)
+    monkeylist[-1].append(falsethrow)
+
+counts = [0 for i in range(len(monkeylist))]
+def round():
+    for m in range(len(monkeylist)):
+        for i in range(len(monkeylist[m][0])):
+            counts[m] += 1
+            curr = monkeylist[m][0][i]
+            if monkeylist[m][1][0] == "+":
+                if monkeylist[m][1][1] == 'old':
+                    curr += curr
+                else:
+                    curr += int(monkeylist[m][1][1])
+            elif monkeylist[m][1][0] == "*":
+                if monkeylist[m][1][1] == 'old':
+                    curr *= curr
+                else:
+                    curr *= int(monkeylist[m][1][1])
+            else:
+                print("invalid state", monkeylist[m], i, curr)
+                return
+            curr //= 3
+            toiftrue = monkeylist[m][3]
+            toiffalse = monkeylist[m][4]
+            if curr % monkeylist[m][2] == 0:
+                monkeylist[toiftrue][0].append(curr)
+            else:
+                monkeylist[toiffalse][0].append(curr)
+        monkeylist[m][0] = []
+
+for i in range(20):
+    print(i)
+    round()
+counts.sort()
+print(counts[-1]*counts[-2])
