1d757964985b16e2bb03fcbb49a587f2dfe25586
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-12 14:27:04 +0100

    do backwards bfs instead of looking through all a's

--- 12.py
+++ 12.py
@@ -1,9 +1,7 @@
 import sys, re
 lines = list(map(str.strip, sys.stdin.readlines()))
-start = [-1, -1]
-end = [-1, -1]
+start, end = [-1, -1], [-1, -1]
 
-starts = [[i, j] for i in range(len(lines)) for j in range(len(lines[i])) if lines[i][j] == 'a']
 for i in range(len(lines)):
     lines[i] = list(lines[i])
     for j in range(len(lines[i])):
@@ -15,7 +13,7 @@ for i in range(len(lines)):
             lines[i][j] = 'z'
 
 dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-def bfs(start):
+def bfs(start, end, p2=False):
     visited = set()
     q = [start]
     level = 0
@@ -26,25 +24,22 @@ def bfs(start):
         for _ in range(len(q)):
             cur = q.pop(0)
             currchar = ord(lines[cur[0]][cur[1]])
+            if p2 and currchar == 97: return level
             for d in dirs:
                 new = [cur[0] + d[0], cur[1] + d[1]]
-                if new[0] >= 0 and new[0] < len(lines) and new[1] >= 0 and new[1] < len(lines[0]):
+                if 0 <= new[0] < len(lines) and 0 <= new[1] < len(lines[0]):
                     otherchar = ord(lines[new[0]][new[1]])
-                    if tuple(new) not in visited and (otherchar - currchar <= 1):
-                        q.append(new)
-                        visited.add(tuple(new))
+                    if tuple(new) not in visited:
+                        if not p2:
+                            if otherchar - currchar <= 1:
+                                q.append(new)
+                                visited.add(tuple(new))
+                        else:
+                            if otherchar - currchar >= -1:
+                                q.append(new)
+                                visited.add(tuple(new))
         level += 1
     return level
-    
-result = 10**9
-for new_start in starts:
-    result = min(result, bfs(new_start))
-print("Part one:", bfs(start))
-print("Part two:", result)
 
-
-
-
-
-
-
+print("Part one:", bfs(start, end))
+print("Part two:", bfs(end, start, True))
