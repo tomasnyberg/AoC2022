1a835439287e2c1b4e5e09073b8d8e2a9a922b18
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-12 15:21:56 +0100

    more minor refactoring

--- 11.py
+++ 11.py
@@ -1,12 +1,11 @@
 import sys, re
-from queue import deque
 monkeys = sys.stdin.read().split("\n\n")
 
 monkey_list = []
 MOD = 1
 for monkey in monkeys:
     _, starting, op, test, t, f = monkey.split("\n")
-    starting = deque(map(int, re.findall(r"\d+", starting)))
+    starting = list(map(int, re.findall(r"\d+", starting)))
     op = op.split(" = ")[1].split(" ")[1:]
     test = int(test.split(" ")[-1])
     MOD *= test
@@ -14,23 +13,19 @@ for monkey in monkeys:
     f = int(f.split(" ")[-1])
     monkey_list.append((starting, op, test, t, f))
 
-def cycle(counts, monkey_list, part_two=False):
-    for idx, m in enumerate(monkey_list):
-        while m[0]:
-            counts[idx] += 1
-            item = m[0].popleft()
-            second = int(m[1][1]) if m[1][1] != 'old' else item
-            item = item * second if m[1][0] == '*' else item + second
-            item = item % MOD if part_two else item // 3
-            if item % m[2] == 0:
-                monkey_list[m[3]][0].append(item)
-            else:
-                monkey_list[m[4]][0].append(item)
-
 def solve(iterations, part_two):
-    monkeys = [(deque([x for x in m[0]]), m[1], m[2], m[3], m[4]) for m in monkey_list]
+    def cycle(counts, monkey_list, part_two=False):
+        for idx, m in enumerate(monkey_list):
+            while m[0]:
+                counts[idx] += 1
+                item = m[0].pop()
+                second = int(m[1][1]) if m[1][1] != 'old' else item
+                item = item * second if m[1][0] == '*' else item + second
+                item = item % MOD if part_two else item // 3
+                monkey_list[m[3 if item % m[2] == 0 else 4]][0].append(item)
+    monkeys = [(list([x for x in m[0]]), m[1], m[2], m[3], m[4]) for m in monkey_list]
     counts = [0]*len(monkey_list)
-    for i in range(iterations):
+    for _ in range(iterations):
         cycle(counts, monkeys, part_two)
     counts.sort()
     return counts[-1] * counts[-2]
