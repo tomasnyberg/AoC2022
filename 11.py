955ce536929e3ce1fda461308d5151a562643b0d
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-12 15:01:46 +0100

    use deque instead to not be stupid

--- 11.py
+++ 11.py
@@ -1,10 +1,11 @@
 import sys, re
+from queue import deque
 monkeys = sys.stdin.read().split("\n\n")
 
 monkey_list = []
 for monkey in monkeys:
     _, starting, op, test, t, f = monkey.split("\n")
-    starting = list(map(int, re.findall(r"\d+", starting)))
+    starting = deque(map(int, re.findall(r"\d+", starting)))
     op = op.split(" = ")[1].split(" ")[1:]
     test = int(test.split(" ")[-1])
     t = int(t.split(" ")[-1])
@@ -16,7 +17,7 @@ def cycle():
     for idx, m in enumerate(monkey_list):
         while m[0]:
             counts[idx] += 1
-            item = m[0].pop(0) # fix
+            item = m[0].popleft()
             second = int(m[1][1]) if m[1][1] != 'old' else item
             item = item * second if m[1][0] == '*' else item + second
             item //= 3
