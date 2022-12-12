0637aa97fe9ab4a079722e361f12eb7495363574
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-12 15:27:09 +0100

    slight performance improvements to day 11, 300 ms faster

--- 11.py
+++ 11.py
@@ -9,19 +9,19 @@ for monkey in monkeys:
     op = op.split(" = ")[1].split(" ")[1:]
     test, t, f = map(lambda x: int(x.split(" ")[-1]), [test, t, f])
     MOD *= test
-    monkey_list.append((starting, op, test, t, f))
+    monkey_list.append([starting, op, test, t, f])
 
 def solve(iterations, part_two):
     def cycle(counts, monkey_list, part_two=False):
         for idx, m in enumerate(monkey_list):
-            while m[0]:
-                counts[idx] += 1
-                item = m[0].pop()
+            counts[idx] += len(m[0])
+            for item in m[0]:
                 second = int(m[1][1]) if m[1][1] != 'old' else item
                 item = item * second if m[1][0] == '*' else item + second
                 item = item % MOD if part_two else item // 3
                 monkey_list[m[3 if item % m[2] == 0 else 4]][0].append(item)
-    monkeys = [(list([x for x in m[0]]), m[1], m[2], m[3], m[4]) for m in monkey_list]
+            m[0] = []
+    monkeys = [[list([x for x in m[0]]), m[1], m[2], m[3], m[4]] for m in monkey_list]
     counts = [0]*len(monkey_list)
     for _ in range(iterations):
         cycle(counts, monkeys, part_two)
