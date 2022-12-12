8b6d457c6e3fa028973624dce8afb240d134e06f
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-12 14:36:44 +0100

    slight clean up day 12

--- 12.py
+++ 12.py
@@ -14,13 +14,12 @@ for i in range(len(lines)):
 
 dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 def bfs(start, end, p2=False):
+    def good_check(p2, other, curr):
+        return (p2 and (other - curr >= -1)) or (not p2 and other - curr <= 1)
     visited = set()
     q = [start]
     level = 0
     while tuple(end) not in visited:
-        if not q:
-            level = 10**9
-            break
         for _ in range(len(q)):
             cur = q.pop(0)
             currchar = ord(lines[cur[0]][cur[1]])
@@ -30,14 +29,9 @@ def bfs(start, end, p2=False):
                 if 0 <= new[0] < len(lines) and 0 <= new[1] < len(lines[0]):
                     otherchar = ord(lines[new[0]][new[1]])
                     if tuple(new) not in visited:
-                        if not p2:
-                            if otherchar - currchar <= 1:
-                                q.append(new)
-                                visited.add(tuple(new))
-                        else:
-                            if otherchar - currchar >= -1:
-                                q.append(new)
-                                visited.add(tuple(new))
+                        if good_check(p2, otherchar, currchar):
+                            q.append(new)
+                            visited.add(tuple(new))
         level += 1
     return level
 
