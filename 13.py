0fdfd442aad932acf691ad75f09ed846b47d374a
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-13 06:38:50 +0100

    somce refactoring

--- 13.py
+++ 13.py
@@ -1,6 +1,5 @@
-import sys, re
-import functools
-pairs = sys.stdin.read().split("\n\n")
+import sys, functools
+
 def score(a, b):
     if type(a) == int and type(b) == int:
         return 0 if a == b else 1 if a < b else -1
@@ -10,27 +9,17 @@ def score(a, b):
                 return score(a[i], b[i])
         return 0 if len(a) == len(b) else 1 if len(a) < len(b) else -1
     return score(a, [b]) if type(a) == list else score([a], b) 
-
-result = 0
+    
+pairs = sys.stdin.read().split("\n\n")
+p1res = 0
 all_packets = [[[2]], [[6]]]
 for j in range(len(pairs)):
-    pair = pairs[j]
-    pair = pair.split("\n")
-    a = pair[0]
-    b = pair[1]
-    a = eval(a)
-    b = eval(b)
-    all_packets.append(a)
-    all_packets.append(b)
-    if score(a, b) == 1:
-        result += j + 1
+    pair = list(map(eval, pairs[j].split("\n")))
+    all_packets += pair
+    if score(pair[0], pair[1]) == 1:
+        p1res += j + 1
 
 all_packets = sorted(all_packets, key=functools.cmp_to_key(lambda a, b: -score(a, b)))
-res = 1
-for i in range(len(all_packets)):
-    if all_packets[i] == [[2]]:
-        res *= i + 1
-    elif all_packets[i] == [[6]]:
-        res *= i + 1
-print("Part one:", result)
+res = 1 * (all_packets.index([[2]]) + 1) * (all_packets.index([[6]]) + 1)
+print("Part one:", p1res)
 print("Part two:", res)
