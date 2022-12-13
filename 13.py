cbb91d07d1515d90e5aed088163c212ef5168687
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-13 06:54:53 +0100

    made day 13 uglier lol

--- 13.py
+++ 13.py
@@ -10,15 +10,11 @@ def score(a, b):
         return 0 if len(a) == len(b) else 1 if len(a) < len(b) else -1
     return score(a, [b]) if type(a) == list else score([a], b) 
 
-pairs = sys.stdin.read().split("\n\n")
-all_packets = [list(map(eval, pair.split("\n"))) for pair in pairs]
-all_packets = [item for sublist in all_packets for item in sublist] + [[[2]], [[6]]]
-p1res = 0
-for i in range(1, len(all_packets) - 2, 2):
-    s = score(all_packets[i-1], all_packets[i])
-    p1res += (i // 2) + 1 if s == 1 else 0
-all_packets = sorted(all_packets, key=functools.cmp_to_key(lambda a, b: -score(a, b)))
-res = 1 * (all_packets.index([[2]]) + 1) * (all_packets.index([[6]]) + 1)
+pkts = [list(map(eval, pair.split("\n"))) for pair in sys.stdin.read().split("\n\n")]
+pkts = [item for sublist in pkts for item in sublist] + [[[2]], [[6]]]
+p1res = sum((i//2) + 1 if score(pkts[i-1], pkts[i]) == 1 else 0 for i in range(1, len(pkts) - 2, 2))
+pkts = sorted(pkts, key=functools.cmp_to_key(lambda a, b: -score(a, b)))
+p2res = 1 * (pkts.index([[2]]) + 1) * (pkts.index([[6]]) + 1)
 
 print("Part one:", p1res)
-print("Part two:", res)
+print("Part two:", p2res)
