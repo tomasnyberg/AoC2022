9b456c361ca30c1aeca2eef794d50ad5b49da9c9
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-13 06:50:59 +0100

    more refactoring

--- 13.py
+++ 13.py
@@ -9,17 +9,16 @@ def score(a, b):
                 return score(a[i], b[i])
         return 0 if len(a) == len(b) else 1 if len(a) < len(b) else -1
     return score(a, [b]) if type(a) == list else score([a], b) 
-    
-pairs = sys.stdin.read().split("\n\n")
-p1res = 0
-all_packets = [[[2]], [[6]]]
-for j in range(len(pairs)):
-    pair = list(map(eval, pairs[j].split("\n")))
-    all_packets += pair
-    if score(pair[0], pair[1]) == 1:
-        p1res += j + 1
 
+pairs = sys.stdin.read().split("\n\n")
+all_packets = [list(map(eval, pair.split("\n"))) for pair in pairs]
+all_packets = [item for sublist in all_packets for item in sublist] + [[[2]], [[6]]]
+p1res = 0
+for i in range(1, len(all_packets) - 2, 2):
+    s = score(all_packets[i-1], all_packets[i])
+    p1res += (i // 2) + 1 if s == 1 else 0
 all_packets = sorted(all_packets, key=functools.cmp_to_key(lambda a, b: -score(a, b)))
 res = 1 * (all_packets.index([[2]]) + 1) * (all_packets.index([[6]]) + 1)
+
 print("Part one:", p1res)
 print("Part two:", res)
