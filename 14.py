2c7d0d8b48afc168eac3aadbbc01d4e450eeaa56
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-14 07:09:52 +0100

    some cleanup to start with

--- 14.py
+++ 14.py
@@ -1,9 +1,7 @@
 import sys, re
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-# 1000 * 1000 matrix
 matrix = [[0 for i in range(1000)] for j in range(1000)]
-
 all_lines = []
 for line in lines:
     split = line.split(" -> ")
@@ -20,21 +18,9 @@ for line in all_lines:
         for x in range(min(x1, x2), max(x1, x2)+1):
             for y in range(min(y1, y2), max(y1, y2)+1):
                 matrix[y][x] = 1
-                # print(y, x)
-print(max_y)
 
 for i in range(len(matrix[max_y+2])):
     matrix[max_y+2][i] = 1
-def print_matrix():
-    for r in range(15):
-        for c in matrix[r][490:505]:
-            if c == 1:
-                print("#", end="")
-            else:
-                print(".", end="")
-        print()
-    # print(*matrix[r][490:505])
-# print(all_lines)
 def sand():
     r = 0
     c = 500
@@ -52,17 +38,13 @@ def sand():
             c+=1
             continue
         matrix[r][c] = 1
-        print("sand landed at", r, c)
-        # print_matrix()
         break
     if r > 980:
         return True
-        
 
-for i in range(100000):
-    if sand():
-        print(i)
-        break
+def solve():
+    for i in range(100000):
+        if sand():
+            return i
 
-            
-
+print("Part two:", solve())
