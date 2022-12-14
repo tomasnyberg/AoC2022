13d8196eda6db066e38ea9be61a97e5c8e92787b
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-14 07:38:50 +0100

    minor refactoring

--- 14.py
+++ 14.py
@@ -3,21 +3,18 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 matrix = set()
 for line in lines:
-    split = line.split(" -> ")
     prev = None
-    for s in split:
-        x, y = list(map(int, re.findall(r'\d+', s)))
+    for x, y in (list(map(int, re.findall(r'\d+', s))) for s in line.split(" -> ")):
         if prev is not None:
             for i in range(min(prev[0], x), max(prev[0], x)+1):
                 for j in range(min(prev[1], y), max(prev[1], y)+1):
                     matrix.add((j, i))
         prev = (x, y)
+
 max_y = max(x for x, y in matrix) + 2
 for i in range(50000):
     matrix.add((max_y, i))
-# def print_matrix():
-#     for i in range(13):
-#         print("".join("#" if (i, j) in matrix else "." for j in range(490, 510)))
+
 def sand():
     p = [0, 500]
     if tuple(p) in matrix: return True
