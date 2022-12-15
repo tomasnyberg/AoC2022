91dd70472931e4d5362d5bb64dcdf21608ad617d
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-15 06:30:27 +0100

    had an off by 1 zzz

--- 15.py
+++ 15.py
@@ -5,45 +5,37 @@ sensors = {}
 beacons = set()
 
 row = 2000000
-maxx = 0
-maxy = 0
-maxd = 0
-minx = 0
-miny= 0
 
 # row = 10
 for line in lines:
-    x1, y1, x2, y2 = map(int, re.findall(r'-?\d+', line))
+    y1, x1, y2, x2 = map(int, re.findall(r'-?\d+', line))
     d = abs(int(x1) - int(x2)) + abs(int(y1) - int(y2))
     sensors[(x1, y1)] = ((x2, y2), d)
     beacons.add((x2, y2))
-    maxx = max(maxx, x1, x2)
-    maxy = max(maxy, y1, y2)
-    maxd = max(maxd, d)
-    minx = min(minx, x1, x2)
-    miny = min(miny, y1, y2)
-
-print(maxx, maxy, minx, miny, maxd)
+# for r, c in sensors:
+#     print(r, c)
 result = 0
 for col in range(-5000000, 5000000):
     if col % 1000000 == 0:
         print(col)
     found = False
-    if (row, col) in beacons:
+    if (row, col) in beacons or ( row, col) in sensors:
         continue
     for s in sensors:
         closest = sensors[s][-1]
-        # manhattan distance between (row, j) and s
-        d = abs(col - s[0]) + abs(row - s[1])
+        d = abs(col - s[1]) + abs(row - s[0])
         if d <= closest:
             found = True
             break
     if found:
+        print(row, col)
         result += 1
-
 print(result)
+
+
+
+
 
 # guessed 6636384
 # guessed 5175617
-
-# should guess 4582668
+# guessed 4582668
