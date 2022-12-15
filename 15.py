fa45330b8739d705c75effc49a5656d7ce5d3d67
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-15 07:06:08 +0100

    need to refactor later, slow code for p2 but worked

--- 15.py
+++ 15.py
@@ -15,27 +15,71 @@ for line in lines:
 # for r, c in sensors:
 #     print(r, c)
 result = 0
-for col in range(-5000000, 5000000):
-    if col % 1000000 == 0:
-        print(col)
-    found = False
-    if (row, col) in beacons or ( row, col) in sensors:
-        continue
+# for col in range(0, 4000000):
+#     if col % 1000000 == 0:
+#         print(col)
+#     found = False
+#     if (row, col) in beacons or ( row, col) in sensors:
+#         continue
+#     for s in sensors:
+#         closest = sensors[s][-1]
+#         d = abs(col - s[1]) + abs(row - s[0])
+#         if d <= closest:
+#             found = True
+#             break
+#     if found:
+#         print(row, col)
+#         result += 1
+MAX = 4000000
+def solve():
+    def check(pos):
+        row, col = pos
+        if row > MAX or col > MAX or row < 0 or col < 0:
+            return False
+        # Check if every sensor is further away than its beacon
+        for s in sensors:
+            r, c, d =  s[0], s[1], sensors[s][-1]
+            if abs(row - r) + abs(col - c) <= d:
+                return False
+        return True
     for s in sensors:
-        closest = sensors[s][-1]
-        d = abs(col - s[1]) + abs(row - s[0])
-        if d <= closest:
-            found = True
-            break
-    if found:
-        print(row, col)
-        result += 1
+        # print(s)
+        r, c, d =  s[0], s[1], sensors[s][-1]
+        # r,c,d = 0,0,1
+        pos = [r - d - 1, c]
+        visited = set()
+        # print(1)
+        while pos[0] < r:
+            if(check(pos)):
+                print(pos)
+            # print(pos)
+            pos[0] += 1
+            pos[1] += 1
+            visited.add((pos[0], pos[1]))
+        # print(2)
+        while pos[1] > c:
+            if(check(pos)):
+                print(pos)
+            # print(pos)
+            pos[0] += 1
+            pos[1] -= 1
+            visited.add((pos[0], pos[1]))
+        # print(3)
+        while pos[0] > r:
+            if(check(pos)):
+                print(pos)
+            # print(pos)
+            pos[0] -= 1
+            pos[1] -= 1
+            visited.add((pos[0], pos[1]))
+        # print(4)
+        while pos[1] <= c:
+            if(check(pos)):
+                print(pos)
+            # print(pos)
+            pos[0] -= 1
+            pos[1] += 1
+            visited.add((pos[0], pos[1]))
+
+solve()
 print(result)
-
-
-
-
-
-# guessed 6636384
-# guessed 5175617
-# guessed 4582668
