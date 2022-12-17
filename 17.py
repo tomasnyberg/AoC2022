ed113df9cb9dcae4771d335bd2dbf41edaae28e4
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-17 10:07:55 +0100

    remade solution for 17

--- 17.py
+++ 17.py
@@ -1,181 +1,88 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-# ###C index 0
-
-# .#.
-# #C# index 1
-# .#.
-
-# ..#
-# ..#
-# ##C index 2
-
-# #
-# #
-# #
-# C index 3
-# 
-# ##
-# #C index 4
-
-dirs = lines[0]*1000
-print(len(dirs))
-
-# 7 units wide, 100 tall grid
-grid = [[0 for _ in range(7)] for _ in range(100000)]
-for xs in (grid[-5:]):
-    print(xs)
-
-def check_down(i, center):
-# index 0 doesn't draw properly
-# index 1 doesn't draw its sides
-# index 4 doesn't draw at all
-    row, col = center
+dirs = lines[0]
+def generate_pieces(i, row):
+    positions = []
     if i == 0:
-        if row < len(grid) - 1 and all(grid[row+1][newcol] == 0 for newcol in range(col-3, col+1)):
-            return [row+1, col]
-        else:
-            for newcol in range(col-3, col+1):
-                grid[row][newcol] = 1 
-            return [row, col]
+        positions = ((2,row), (3,row), (4,row), (5,row))
     elif i == 1:
-        if row < len(grid) - 2 and grid[row+1][col+1] == 0 and grid[row+1][col-1] == 0 and grid[row+2][col] == 0:
-            return [row+1, col] 
-        else:
-            grid[row][col] = 1
-            grid[row][col+1] = 1
-            grid[row][col-1] = 1
-            grid[row-1][col] = 1
-            grid[row+1][col] = 1
-            return [row, col]
+        positions = ((3,row), (3,row-1), (3, row-2), (2, row-1), (4, row-1))
     elif i == 2:
-        if row < len(grid) - 1 and all(grid[row+1][newcol] == 0 for newcol in range(col-2, col+1)):
-            return [row+1, col]
-        else:
-            for newcol in range(col-2, col+1):
-                grid[row][newcol] = 1
-            grid[row-1][col] = 1
-            grid[row-2][col] = 1
-            return [row, col]
+        positions = ((2,row), (3,row), (4,row), (4, row-1), (4, row-2))
     elif i == 3:
-        if row < len(grid) - 1 and grid[row+1][col] == 0:
-            return [row+1, col]
-        else:
-            for newrow in range(row-3, row+1):
-                grid[newrow][col] = 1
-            return [row, col]
+        positions = ((2,row), (2,row-1), (2,row-2), (2,row-3))
     elif i == 4:
-        if row < len(grid) - 1 and grid[row+1][col] == 0 and grid[row+1][col-1] == 0:
-            return [row+1, col]
-        else:
-            print("4 landed at", row, col)
-            grid[row][col] = 1
-            grid[row][col-1] = 1
-            grid[row-1][col-1] = 1
-            grid[row-1][col] = 1
-            return [row, col]
+        positions = ((2,row), (3,row), (2,row-1), (3,row-1))
     else:
-        print(i)
-        raise Exception("Invalid i")
+        assert(False)
+    return set(positions)
 
-def check_side(i, center, dir):
-    row, col = center
-    if i == 0:
-        if dir == '<':
-            if col - 3 > 0 and grid[row][col-4] == 0:
-                return [row, col-1]
-            else:
-                return [row, col]
-        elif dir == '>':
-            if col + 1 < len(grid[0]) and grid[row][col+1] == 0:
-                return [row, col+1]
-            else:
-                return [row, col]
-    elif i == 1:
-        if dir == '<':
-            if col - 1 > 0 and grid[row][col-2] == 0 and grid[row+1][col-1] == 0 and grid[row-1][col-1] == 0:
-                return [row, col-1]
-            else:
-                return [row, col]
-        elif dir == '>':
-            if col + 2 < len(grid[0]) and grid[row][col+2] == 0 and grid[row+1][col+1] == 0 and grid[row-1][col+1] == 0:
-                return [row, col+1]
-            else:
-                return [row, col]
-    elif i == 2:
-        if dir == '<':
-            if col - 2 > 0 and grid[row][col-3] == 0 and grid[row-1][col-1] == 0 and grid[row-2][col-1] == 0:
-                return [row, col-1]
-            else:
-                return [row, col]
-        elif dir == '>':
-            if col + 1< len(grid[0]) and grid[row][col+1] == 0 and grid[row-1][col+1] == 0 and grid[row-2][col+1] == 0:
-                return [row, col+1]
-            else:
-                return [row, col]
-    elif i == 3:
-        if dir == '<':
-            # THIS LINE COULD BE WRONG
-            if col - 1 > 0 and all(grid[newrow][col-1] == 0 for newrow in range(row-3, row+1)):
-                return [row, col-1]
-            else:
-                return [row, col]
-        elif dir == '>':
-            if col + 1 < len(grid[0]) and all(grid[newrow][col+1] == 0 for newrow in range(row-3, row+1)):
-                return [row, col+1]
-            else:
-                return [row, col]
-    elif i == 4:
-        if dir == '<':
-            if col - 1 > 0 and grid[row][col-2] == 0 and grid[row-1][col-2] == 0:
-                return [row, col-1]
-            else:
-                return [row, col]
-        elif dir == '>':
-            if col + 1 < len(grid[0]) and grid[row][col+1] == 0 and grid[row-1][col+1] == 0:
-                return [row, col+1]
-            else:
-                return [row, col]
+grid = [[0 for _ in range(7)] for _ in range(5000)]
+
+def move_down(piece):
+    if all(y+1 < len(grid) for x, y in piece) and all(grid[y+1][x] == 0 or (x, y+1) in piece for x, y in piece):
+        return [True, set((x, y+1) for x, y in piece)]
     else:
-        print(i)
-        raise Exception("Invalid i")
-# Not 100% sure about these
-startposes = {0: [0, 5], 1: [1,3], 3: [3, 2], 2:[2,4], 4:[1, 3]}
+        for x, y in piece:
+            grid[y][x] = 1
+        return [False, piece]
 
-def print_grid():
-    for row in grid[-15:]:
-        print(''.join(['#' if x == 1 else '.' for x in row]))
-    print("-------------")
+def move_side(piece, dir):
+    dx = 1 if dir == ">" else -1
+    if all(0 <= x+dx < len(grid[0]) for x, y in piece) and all(grid[y][x+dx] == 0 or (x+dx, y) in piece for x, y in piece):
+        return set((x+dx, y) for x, y in piece)
+    else:
+        return piece
 
-highest = len(grid)
-diridx = 0
-for i in range(2022):
-    startpos = startposes[i % 5]
-    startpos[0] = highest - 4
-    if i % 5 == 1:
-        startpos[0] -= 1
-    while True:
-        # print(startpos, "asdlfk")
-        startpos = check_side(i%5, startpos, dirs[diridx])
-        diridx+=1
-        diridx %= len(dirs)
-        newpos = check_down(i%5, startpos)
-        if newpos == startpos:
-            imodded = i % 5
-            if imodded == 0:
-                highest = min(highest, startpos[0])
-            elif imodded == 1 or imodded == 4:
-                highest = min(highest, startpos[0] - 1)
-            elif imodded == 2:
-                highest = min(highest, startpos[0] - 2)
-            elif imodded == 3:
-                highest = min(highest, startpos[0] - 3)
-            # print_grid()
-            break
-        else:
-            startpos = newpos
-print(len(grid) - highest)
+def draw_intermediate(grid, piece):
+    grid = [list(xs) for xs in grid]
+    for x, y in piece:
+        grid[y][x] = 1
+    for xs in grid[-10:]:
+        print("".join("#" if x == 1 else "." for x in xs))
+    print("\n")
 
-# guessed 3272, too high
+def signature(streamidx, rockidx, grid, height):
+    return (streamidx, rockidx, tuple(tuple(xs) for xs in grid[height:height+10]))
+
+def find_first():
+    heights = []
+    seen = {}
+    highest = len(grid)
+    idx = 0
+    additional = 0
+    i = 0
+    added = False
+    actual = 0
+    while i < 1000000000000:
+        piece = generate_pieces(i%5, highest - 4)
+        while piece:
+            dir = dirs[idx]
+            idx = (idx + 1) % len(dirs)
+            piece = move_side(piece, dir)
+            good, piece = move_down(piece)
+            if not good:
+                highest = min(highest, min(y for x, y in piece))
+                heights.append(highest)
+                if not added:
+                    sig = signature(idx, i%5, grid, highest)
+                    if sig in seen:
+                        additional = ((1000000000000 - i) // (i - seen[sig][1])) * (seen[sig][0] - highest)
+                        i += ((1000000000000 - i) // (i - seen[sig][1])) * (i - seen[sig][1])
+                        added = True
+                    seen[sig] = (highest, i)
+                break
+        i += 1
+        actual += 1
+        if actual == 2022:
+            print("Part one:", len(grid) - highest)
+    print("Part two:", additional+ len(grid) - highest)
+
+find_first()
+# print(len(grid) - highest)
+
+
+        
+
 
