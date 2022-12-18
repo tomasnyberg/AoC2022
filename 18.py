d46bfafd62fae8784d8a5c74c565fae1387f96e1
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-18 11:19:35 +0100

    redo solution slightly for p2

--- 18.py
+++ 18.py
@@ -1,37 +1,31 @@
 import sys, re
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-# 100 by 100 by 100 grid
-grid = [[[0 for k in range(100)] for j in range(100)] for i in range(100)]
-# All dirs in 6 directions
-dirs = [(1,0,0),(-1,0,0),(0,1,0),(0,-1,0),(0,0,1),(0,0,-1)]
 coordinates = set()
 for line in lines:
-    a, b, c = map(int, re.findall(r'-?\d+', line))
-    coordinates.add((a,b,c))
-    grid[a][b][c] = 1
+    x, y, z = map(int, re.findall(r'-?\d+', line))
+    coordinates.add((x, y, z))
+
+dirs = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
 result = 0
 for x, y, z in coordinates:
-    result += 6
     for dx, dy, dz in dirs:
-        if grid[x+dx][y+dy][z+dz] == 1:
-            result -=1 
-every = set((x,y,z) for x in range(100) for y in range(100) for z in range(100))
-empty = every - coordinates
-def bfs():
-    q = [(0,0,0)]
-    while q:
-        c = q.pop()
-        if c in empty:
-            empty.remove(c)
-            q.extend([(c[0]+dx, c[1]+dy, c[2]+dz) for dx, dy, dz in dirs])
-bfs()
-for c in empty:
-    result += 6
-    for dx, dy, dz in dirs:
-        newcoords = (c[0]+dx, c[1]+dy, c[2]+dz)
-        if newcoords in coordinates:
-            result -= 2
-    coordinates.add(c)
+        if (x + dx, y + dy, z + dz) not in coordinates:
+            result += 1
             
-print(result)
+visited = set()
+result2 = set()
+q = [(0, 0, 0)]
+while q:
+    x, y, z = q.pop()
+    visited.add((x, y, z))
+    for dx, dy, dz in dirs:
+        if not (-5 <= x + dx <= 30 and -5 <= y + dy <= 30 and -5 <= z + dz <= 30):
+            continue
+        if (x + dx, y + dy, z + dz) in coordinates:
+            result2.add((x,y,z,x + dx, y + dy, z + dz))
+        elif (x + dx, y + dy, z + dz) not in visited:
+            q.append((x + dx, y + dy, z + dz))
+
+print("Part one:", result)
+print("Part two:", len(result2))
