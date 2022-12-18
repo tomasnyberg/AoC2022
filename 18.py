dc740c54fcfb61ba69ceacb231d08c3549ca8109
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-18 06:28:59 +0100

    p2 incredibly messy

--- 18.py
+++ 18.py
@@ -5,14 +5,33 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 grid = [[[0 for k in range(100)] for j in range(100)] for i in range(100)]
 # All dirs in 6 directions
 dirs = [(1,0,0),(-1,0,0),(0,1,0),(0,-1,0),(0,0,1),(0,0,-1)]
-coordinates = []
+coordinates = set()
 for line in lines:
     a, b, c = map(int, re.findall(r'-?\d+', line))
-    coordinates.append((a,b,c))
+    coordinates.add((a,b,c))
     grid[a][b][c] = 1
 result = 0
 for x, y, z in coordinates:
-    for i, j, k in dirs:
-        if grid[x+i][y+j][z+k] == 0:
-            result += 1
+    result += 6
+    for dx, dy, dz in dirs:
+        if grid[x+dx][y+dy][z+dz] == 1:
+            result -=1 
+every = set((x,y,z) for x in range(100) for y in range(100) for z in range(100))
+empty = every - coordinates
+def bfs():
+    q = [(0,0,0)]
+    while q:
+        c = q.pop()
+        if c in empty:
+            empty.remove(c)
+            q.extend([(c[0]+dx, c[1]+dy, c[2]+dz) for dx, dy, dz in dirs])
+bfs()
+for c in empty:
+    result += 6
+    for dx, dy, dz in dirs:
+        newcoords = (c[0]+dx, c[1]+dy, c[2]+dz)
+        if newcoords in coordinates:
+            result -= 2
+    coordinates.add(c)
+            
 print(result)
