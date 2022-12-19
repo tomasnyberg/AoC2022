d63bad21a328451a492601f06cd65e09c153d628
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-19 11:03:40 +0100

    abstract out generating new states

--- 19.py
+++ 19.py
@@ -20,13 +20,27 @@ def apply_heuristic(time, depth, queue):
         depth = time
     return depth, queue
 
-def generate_states(state):
+def new_states(state, costs, oldstate, queue):
+    orecurr,clay_curr,obcurr,geodecurr,oremachine,claymachine,obsidianmachine,geodemachine,time = oldstate
+    ore_cost, clay_cost, obsidian_ore, obsidian_clay, geode_ore, geode_obsidian = costs
     new_states = [list(state) for _ in range(5)]
     for i in range(4):
         for s in new_states:
             s[i] += s[i+4]
     for s in new_states:
         s[8] -= 1
+    for i, pred, costs, add in [[0, True, [0,0,0], [0,0,0,0]],
+                                [1, orecurr >= ore_cost, [ore_cost,0,0], [1,0,0,0]],
+                                [2, orecurr >= clay_cost, [clay_cost, 0,0], [0,1,0,0]],
+                                [3, orecurr >= obsidian_ore and clay_curr >= obsidian_clay, [obsidian_ore,obsidian_clay,0], [0,0,1,0]],
+                                [4, orecurr >= geode_ore and obcurr >= geode_obsidian, [geode_ore,0,geode_obsidian], [0,0,0,1]]]:
+        if not pred: continue
+        for j, base in enumerate([orecurr + oremachine, clay_curr + claymachine, obcurr + obsidianmachine]):
+            new_states[i][j] = base - costs[j]
+        for j in range(len(add)):
+            if add[j]:
+                new_states[i][j+4] += 1
+        queue.append(tuple(new_states[i]))
     return new_states
 
 def prune(state, maxore, obsidian_clay, geode_obsidian):
@@ -45,7 +59,8 @@ def bfs(ore_cost, clay_cost, obsidian_ore, obsidian_clay, geode_ore, geode_obsid
     depth = start_time
     while queue:
         state = queue.popleft()
-        orecurr,clay_curr,obcurr,geodecurr,oremachine,claymachine,obsidianmachine,geodemachine,time = state
+        geodecurr,time = state[3], state[-1]
+        oldstate = state[:]
         depth, queue = apply_heuristic(time, depth, queue)
         best = max(best, geodecurr)
         if time==0: continue
@@ -53,19 +68,8 @@ def bfs(ore_cost, clay_cost, obsidian_ore, obsidian_clay, geode_ore, geode_obsid
         state = prune(state, maxore, obsidian_clay, geode_obsidian)
         if state in seen: continue
         seen.add(state)
-        new_states = generate_states(state)
-        for i, pred, costs, add in [[0, True, [0,0,0], [0,0,0,0]],
-                                    [1, orecurr >= ore_cost, [ore_cost,0,0], [1,0,0,0]],
-                                    [2, orecurr >= clay_cost, [clay_cost, 0,0], [0,1,0,0]],
-                                    [3, orecurr >= obsidian_ore and clay_curr >= obsidian_clay, [obsidian_ore,obsidian_clay,0], [0,0,1,0]],
-                                    [4, orecurr >= geode_ore and obcurr >= geode_obsidian, [geode_ore,0,geode_obsidian], [0,0,0,1]]]:
-            if not pred: continue
-            for j, base in enumerate([orecurr + oremachine, clay_curr + claymachine, obcurr + obsidianmachine]):
-                new_states[i][j] = base - costs[j]
-            for j in range(len(add)):
-                if add[j]:
-                    new_states[i][j+4] += 1
-            queue.append(tuple(new_states[i]))
+        costs = [ore_cost, clay_cost, obsidian_ore, obsidian_clay, geode_ore, geode_obsidian]
+        new_states(state, costs, oldstate, queue)
     print(best)
     return best
 
