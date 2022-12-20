76da1ccde05cd02d9cbbf4f3437b3bb6488dfb2a
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-20 07:05:43 +0100

    some cleanup

--- 20.py
+++ 20.py
@@ -1,29 +1,21 @@
 import sys, re
 lines = list(map(str.strip, sys.stdin.readlines()))
-KEY = 811589153
 
 nums = []
 for idx, line in enumerate(lines):
-    nums.append((int(line)*KEY, idx))
+    nums.append((int(line)*811589153, idx))
 n = len(nums)
-
 original = nums[:]
 
 def move(nums, i):
     x, idx = nums[i]
-    # nums[i] = (x, i)
     nums.pop(i)
-    new = (i + x) % (n-1)
-    nums.insert(new, (x, idx))
+    nums.insert((i + x) % (n-1), (x, idx))
 
 for _ in range(10):
-    print([nums[i] for i in range(len(nums))])
     for x, idx in original:
-        # print("moving", idx)
         i = nums.index((x, idx))
         move(nums, i)
-    print([nums[i] for i in range(len(nums))])
-    print()
 
 zeroindex = 0
 for i in range(n):
