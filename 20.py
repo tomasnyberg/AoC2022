3d71ca2b89e2881d6986095a94e1a1212c702474
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-20 06:44:23 +0100

    working on p2

--- 20.py
+++ 20.py
@@ -1,62 +1,34 @@
 import sys, re
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-#Initial arrangement:
-# 1, 2, -3, 3, -2, 0, 4
-
-# 1 moves between 2 and -3:
-# 2, 1, -3, 3, -2, 0, 4
-
-# 2 moves between -3 and 3:
-# 1, -3, 2, 3, -2, 0, 4
-
-# -3 moves between -2 and 0:
-# 1, 2, 3, -2, -3, 0, 4
-
-# 3 moves between 0 and 4:
-# 1, 2, -2, -3, 0, 3, 4
-
-# -2 moves between 4 and 1:
-# 1, 2, -3, 0, 3, 4, -2
-
-# 0 does not move:
-# 1, 2, -3, 0, 3, 4, -2
-
-# 4 moves between -3 and 0:
-# 1, 2, -3, 4, 0, 3, -2
-
-# Write a function that moves the numbers as described above
-# and returns the new list.
-
-
 nums = []
-for line in lines:
-    nums.append(int(line))
-
-for i in range(len(nums)):
-    nums[i] = (nums[i], False)
+for idx, line in enumerate(lines):
+    nums.append((int(line), idx, False))
+n = len(nums)
 
 def move(nums, i):
-    print(nums)
     x = nums[i][0]
-    if x < 0:
-        newpos = (i + x - 1) % len(nums)
-    else:
-        newpos = (i + x ) % len(nums)
+    nums[i] = (x, i, True)
     nums.pop(i)
-    nums.insert(newpos, (x, True)) 
-    print(nums)
-    print()
+    new = (i + x) % (n-1)
+    nums.insert(new, (x, i, True))
 
 while True:
     moved = False
     for i in range(len(nums)):
-        if not nums[i][1]:
+        if not nums[i][2]:
             move(nums, i)
             moved = True
             break
     if not moved:
         break
 
-print(nums)
-
+zeroindex = 0
+for i in range(n):
+    if nums[i][0] == 0:
+        zeroindex = i
+        break
+total = 0
+for x in [1000, 2000, 3000]:
+    total += nums[(zeroindex+x)%(n)][0]
+print(total)
