d0cfc3d7dca80aa5c1fb50f864ba7929ef7ef1b9
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-20 06:12:48 +0100

    working on p1

--- 20.py
+++ 20.py
@@ -1,6 +1,62 @@
 import sys, re
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+#Initial arrangement:
+# 1, 2, -3, 3, -2, 0, 4
+
+# 1 moves between 2 and -3:
+# 2, 1, -3, 3, -2, 0, 4
+
+# 2 moves between -3 and 3:
+# 1, -3, 2, 3, -2, 0, 4
+
+# -3 moves between -2 and 0:
+# 1, 2, 3, -2, -3, 0, 4
+
+# 3 moves between 0 and 4:
+# 1, 2, -2, -3, 0, 3, 4
+
+# -2 moves between 4 and 1:
+# 1, 2, -3, 0, 3, 4, -2
+
+# 0 does not move:
+# 1, 2, -3, 0, 3, 4, -2
+
+# 4 moves between -3 and 0:
+# 1, 2, -3, 4, 0, 3, -2
+
+# Write a function that moves the numbers as described above
+# and returns the new list.
+
+
+nums = []
 for line in lines:
-    print(line)
-    
+    nums.append(int(line))
+
+for i in range(len(nums)):
+    nums[i] = (nums[i], False)
+
+def move(nums, i):
+    print(nums)
+    x = nums[i][0]
+    if x < 0:
+        newpos = (i + x - 1) % len(nums)
+    else:
+        newpos = (i + x ) % len(nums)
+    nums.pop(i)
+    nums.insert(newpos, (x, True)) 
+    print(nums)
+    print()
+
+while True:
+    moved = False
+    for i in range(len(nums)):
+        if not nums[i][1]:
+            move(nums, i)
+            moved = True
+            break
+    if not moved:
+        break
+
+print(nums)
+
