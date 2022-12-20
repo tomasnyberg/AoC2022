d98193cc2ada52b86eee2baffba7e7122d3f5923
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-20 07:47:14 +0100

    do both parts

--- 20.py
+++ 20.py
@@ -1,7 +1,7 @@
 import sys, re
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-nums= [(int(line)*811589153, idx) for idx, line in enumerate(lines)]
+nums= [(int(line), idx) for idx, line in enumerate(lines)]
 n, original = len(nums), nums[:]
 
 def move(nums, i):
@@ -9,10 +9,27 @@ def move(nums, i):
     nums.pop(i)
     nums.insert((i + x) % (n-1), (x, idx))
 
-for round in range(10):
+def mix():
     for x, idx in original:
         i = nums.index((x, idx))
         move(nums, i)
 
-zeroindex = [x for x, _ in nums].index(0)
-print("Part two:", sum([nums[(zeroindex+x)%(n)][0] for x in [1000, 2000, 3000]]))
+def part_one():
+    mix()
+    zeroindex = [x for x, _ in nums].index(0)
+    print("Part one:", sum([nums[(zeroindex+x)%(n)][0] for x in [1000, 2000, 3000]]))
+
+def part_two():
+    global original
+    global nums
+    nums = original[:]
+    for i in range(len(nums)):
+        nums[i] = (nums[i][0] * 811589153, nums[i][1])
+    original = nums[:]
+    for _ in range(10):
+        mix()
+    zeroindex = [x for x, _ in nums].index(0)
+    print("Part two:", sum([nums[(zeroindex+x)%(n)][0] for x in [1000, 2000, 3000]]))
+
+part_one()
+part_two()
