d92d30809ecb40a705569cd1a0d14b70c0da41a2
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-21 06:12:39 +0100

    gonna bs

--- 21.py
+++ 21.py
@@ -11,24 +11,40 @@ for line in lines:
     else:
         unknown[name] = op
 
-while True:
-    progress = False
-    to_del = []
-    for name, val in unknown.items():
-        a, operation, b = val.split()
-        if a in known and b in known:
-            a = str(known[a])
-            b = str(known[b])
-            known[name] = eval(a + operation + b)
-            to_del.append(name)
-            print("evaluating", name, val, known[name])
-            progress = True
-        else:
-            continue
-    if not to_del:
+def attempt_eval(num, known, unknown):
+    known['humn'] = num
+    while True:
+        progress = False
+        to_del = []
+        for name, val in unknown.items():
+            a, operation, b = val.split()
+            if a in known and b in known:
+                if name == 'root':
+                    print("EVALUATING ROOT", num, known[a], known[b])
+                    return known[a] == known[b]
+                a = str(known[a])
+                b = str(known[b])
+                known[name] = eval(a + operation + b)
+                to_del.append(name)
+                # print("evaluating", name, val, known[name])
+                progress = True
+            else:
+                continue
+        if not to_del:
+            break
+        for name in to_del:
+            del unknown[name]
+    
+
+
+
+for i in range(10000):
+    # copy known and unknown
+    known_copy = known.copy()
+    unknown_copy = unknown.copy()
+    if attempt_eval(i, known_copy, unknown_copy):
+        print("FOUND", i)
         break
-    for name in to_del:
-        del unknown[name]
 print(known)
 print(unknown)
 print(known['root'])
